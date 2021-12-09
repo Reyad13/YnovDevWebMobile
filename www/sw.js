93d302ff97849e2c08a009a45acab896de673028
Bouzeboudja <bouzeboudja.reyad@gmail.com>
2021-12-09 14:21:33 +0100

    modification

--- www/sw.js
+++ www/sw.js
@@ -1,26 +1,10 @@
 /** VARS */
-const ORIGIN_URL = `${location.protocol}//${location.host}`;
-const CACHE_NAME = "offline-v3";
+const CACHE_NAME = "offline";
 const OFFLINE_URL = "offline.html";
-const CACHED_FILES = [
-  OFFLINE_URL,
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js",
-  `${ORIGIN_URL}/css/index.css`,
-  `${ORIGIN_URL}/js/index.js`,
-  `${ORIGIN_URL}/img/logo.png`,
-];
+
 /** FUNCTIONS */
 
 /** Fetch */
-
-const sendOfflinePage = (resolve) => {
-  caches.open(CACHE_NAME).then((cache) => {
-    cache.match(OFFLINE_URL).then((cachedResponse) => {
-      resolve(cachedResponse);
-    });
-  });
-};
 
 const respondWithFetchPromiseNavigate = (event) =>
   new Promise((resolve) => {
@@ -36,24 +20,38 @@ const respondWithFetchPromiseNavigate = (event) =>
             resolve(networkResponse);
           })
           // send cache offline.html
-          .catch(() => sendOfflinePage(resolve));
+          .catch(() => {
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.match(OFFLINE_URL).then((cachedResponse) => {
+                resolve(cachedResponse);
+              });
+            });
+          });
       })
-      .catch(() => sendOfflinePage(resolve));
+      .catch(() => {
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.match(OFFLINE_URL).then((cachedResponse) => {
+            resolve(cachedResponse);
+          });
+        });
+      });
   });
 
-const fetchSW = (event) => {
-  // We only want to call event.respondWith() if this is a navigation request
-  // for an HTML page.
-  if (event.request.mode === "navigate") {
-    event.respondWith(respondWithFetchPromiseNavigate(event));
-  } else if (CACHED_FILES.includes(event.request.url)) {
-    event.respondWith(caches.match(event.request));
-  }
-};
+  const fetchSW = (event) => {
+    // We only want to call event.respondWith() if this is a navigation request
+    // for an HTML page.
+    if (event.request.mode === "navigate") {
+      event.respondWith(respondWithFetchPromiseNavigate(event));
+    } else if (CACHED_FILES.includes(event.request.url)) {
+      event.respondWith(caches.match(event.request));
+    }
+  };
+  
 
 /*********************************** */
 
-/** Activate */
+
+const CACHE_NAME = "offline-v2";
 const deleteOldCaches = () =>
   new Promise((resolve) => {
     caches.keys().then((keys) => {
@@ -67,16 +65,15 @@ const deleteOldCaches = () =>
     });
   });
 
+  /** Activate */
 const waitUntilActivatePromise = () =>
   new Promise((resolve) => {
     deleteOldCaches().then(() => {
-      // Enable navigation preload if it's supported.
-      // See https://developers.google.com/web/updates/2017/02/navigation-preload
       if ("navigationPreload" in self.registration) {
-        self.registration.navigationPreload.enable().finally(resolve);
-      }
-    });
-  });
+        self.registration.navigationPreload.enable().finally(resolve);}});});
+
+
+
 
 const activate = (event) => {
   event.waitUntil(waitUntilActivatePromise());
@@ -86,13 +83,22 @@ const activate = (event) => {
 
 /*********************************** */
 
-/** Install */
+const ORIGIN_URL = `${location.protocol}//${location.host}`;
+const CACHED_FILES = [
+  OFFLINE_URL,
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js",
+  `${ORIGIN_URL}/css/index.css`,
+  `${ORIGIN_URL}/js/index.js`,
+  `${ORIGIN_URL}/img/logo.png`,
+];
 const waitUntilInstallationPromise = () =>
   new Promise((resolve) => {
     caches.open(CACHE_NAME).then((cache) => {
       cache.addAll(CACHED_FILES).then(resolve);
     });
   });
+
 
 const installSW = (event) => {
   event.waitUntil(waitUntilInstallationPromise());
